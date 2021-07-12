346d3d28ee26c4af536cc5f186fb5655f03620b1
Megan McCarty <megan_mccarty@hotmail.com>
2021-07-12 14:52:29 -0400

    commit tree f89111b9753e3acde7161be51dce44796e3d28ff

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
+sample_array = [1, 2, 3]
+
+# Using #each method
 def square_array(array)
-  # your code here
+    new_array = []
+    array.each do |element|
+        new_array.push(element * element)
+    end
+    new_array
 end
+
+# Using #collect method
+def square_array_collect(array)
+    array.collect do |element|
+        element * element
+    end
+end
+
+puts square_array(sample_array)
+puts square_array_collect(sample_array)
